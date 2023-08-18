0bdf984a7a44dbe26f5472d3ab6393cf55677e28
DM <danymartinezluna@gmail.com>
2023-08-18 17:48:55 -0400

    Cleand up files

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import { useState } from 'react'
-
+import Navbar from "./components/Navbar";
 
 function App() {
-  
-
   return (
-    <div><h4>hello</h4></div>
-  )
+    <div>
+      <Navbar/>
+    </div>
+  );
 }
 
-export default App
+export default App;
