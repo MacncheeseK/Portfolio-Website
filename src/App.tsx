1660f85ee5e577a16f109ec051830108bc405346
DM <danymartinezluna@gmail.com>
2023-09-03 10:20:10 -0400

    trying to figure out scrolling text

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import Navbar from './components/Navbar';
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
 
 const App = () => {
   return (
-    <div>
+    <section>
       <Navbar />
-    </div>
+      <section>
+        <Hero/>
+      </section>
+    </section>
   );
 };
 
