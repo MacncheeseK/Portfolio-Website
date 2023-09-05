17a9922b8ef0274f03cb8bffb430fc0d771861bb
DM <danymartinezluna@gmail.com>
2023-09-05 12:36:25 -0400

    updated navbar and added animation

--- src/components/HamburgerMenu.tsx
+++ src/components/HamburgerMenu.tsx
@@ -1,8 +1,19 @@
-export const HamburgerMenu =()=>{
-  return (<div>
-    <div>
+import hamburger from '../assets/hamburger.svg'
+import { motion, spring, transform } from 'framer-motion';
 
+export const HamburgerMenu =()=>{
+
+  return (
+    <div className="mr-8">
+      <div>
+        <motion.button
+          animate={{ rotate: 180, scale: 1 }}
+          transition={{ type: 'spring', damping: 20, stiffness: 280 }}
+        >
+          <img src={hamburger} alt="hamburger menu" className="w-10 h-10 " />
+        </motion.button>
+      </div>
     </div>
-  </div>)
-}
+  );
+} 
 export default HamburgerMenu
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,8 +2,22 @@ import HamburgerMenu from './HamburgerMenu';
 
 export const Navbar = () => {
   return (
-    <nav className="navbar">
-      <h1 className='font-Yapari text-lg p-2 '>Macncheese</h1>
+    <nav className="flex justify-between">
+      <h1 className="font-Yapari text-lg p-2 ">Macncheese</h1>
+      <ul className="flex gap-6 items-center text-xl ">
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/about">About Me</a>
+        </li>
+        <li>
+          <a href="/projects">Projects</a>
+        </li>
+        <li>
+          <a href="/contact">Contact Me</a>
+        </li>
+      </ul>
       <HamburgerMenu />
     </nav>
   );
